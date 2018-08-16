349bcf20fdc58b50bb92d5f171225a230adbe4cf
Unknown <mauricederidder@outlook.com>
2018-08-16 18:52:08 +0200

    Fix merge error.
    Name issue has been resolved.
    No further problems.

--- public/highscores.php
+++ public/highscores.php
@@ -2,7 +2,10 @@
 require('../app/config/DatabaseConnector.php');
 session_start();
 
+$getHighscoresQuery = "SELECT `score` , `name` FROM `tbl_highscores` WHERE `id` <= 10 ORDER BY `tbl_highscores`.`score` DESC"; // we willen natuurlijk niet alle scores laten zien, alleen de hoeveelheid dat de gebruiker aangeeft.
 
+$Highscores = $database->query($getHighscoresQuery)->fetchAll();
+$scoreString= " score:  ";
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,34 +15,23 @@ session_start();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
 
 <div class="container">
-    <pre>
-    <?php
-
-        $getHighscoresQuery = "SELECT `name` , `highscore` FROM `tbl_highscores` WHERE `id` <= 10"; // we willen natuurlijk niet alle scores laten zien, alleen de hoeveelheid dat de gebruiker aangeeft.
-
-        $Highscores = $database->query($getHighscoresQuery)->fetchAll();
-
-        var_dump($Highscores[0]);
-        var_dump($Highscores);
-        ?>
-    </pre>
-        <ul>
-        <?
-        foreach($Highscores as $scores)       // nu laten we voor iedere
-        {
-            var_dump($scores);
-        ?>
-
-            <li></li>
-         <?php
+    <ol>
+        <?php
+        foreach($Highscores as $score) {
+            ?>
+            <li> <div class="score"><?php echo $score["name"].$scoreString.$score["score"]?></li>
+            <?php
         }
         ?>
-        </ul>
-</div>
+    </ol>
 
+</div>
 </body>
 </html>
