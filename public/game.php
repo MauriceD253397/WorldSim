e36ca28e5b916ebfd2d31e9710b1ad80e687e82e
MrRedstoneWolf <stijn@sharks.nl>
2018-08-21 18:48:23 +0200

    9.2 Login And Game Session Required

--- public/game.php
+++ public/game.php
@@ -1,12 +1,33 @@
 <?php
 require('../app/config/DatabaseConnector.php');
 session_start();
-$game_id = $_SESSION['game'];
-foreach ($game_id as $gID) {
-  echo $gID['game_id'];
+if (isset($_SESSION['login']))
+{
+  if (isset($_SESSION['game']))
+  {
+    $game = $_SESSION['game'];
+    $getGameID = "SELECT `tbl_savegames`.`game_id` FROM `tbl_savegames` WHERE `tbl_savegames`.`game_id` = $game ORDER BY `tbl_savegames`.`game_id` ASC";
+    $game_id = $database->query($getGameID)->fetchAll();
+    foreach ($game_id as $gID) {
+      // ...
+    }
+    ?>
+
+    // the game
+
+    // quit game session
+
+    <?php
+  }
+  else
+  <script type='text/javascript'>
+      setTimeout(function () {
+          window.location.replace("../public/savegames.php");
+      },0);</script>
 }
-$user_id = $_SESSION['login'];
-foreach ($user_id as $uID) {
-  echo $uID['id'];
-}
+else
+<script type='text/javascript'>
+    setTimeout(function () {
+        window.location.replace("../public/login.php");
+    },0);</script>
 ?>
