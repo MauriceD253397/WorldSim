89d85df24a7432ddeec329716e4be077c3fd5ae7
MrRedstoneWolf <stijn@sharks.nl>
2018-08-22 18:24:35 +0200

    8.16 Add Creation Date And Last Opened Date

--- app/NewSaveHandler.php
+++ app/NewSaveHandler.php
@@ -27,11 +27,38 @@ function getSaveAmount($save_name, $database)
     return count($resultsUncounted);
 }
 
-if(getSaveAmount($save_name,$database) < 1)
-{
-    $save_nameNotExist = true;
-}
-else{
+  if(getSaveAmount($save_name,$database) < 1)
+  {
+      $save_nameNotExist = true;
+  }
+  else{
+      ?>
+      <script type='text/javascript'>
+          setTimeout(function () {
+              window.location.replace("../public/savegames.php");
+          },0);</script>
+      <?php
+  }
+
+
+  $user_login = $_SESSION['login'];
+  $getUserID = "SELECT * FROM `tbl_login` WHERE `tbl_login`.`username` = '$user_login'";
+  $user_id = $database->query($getUserID)->fetchAll();
+  foreach ($user_id as $uid) {
+    $userIDNumber = $uid["id"];
+    $getExistingSaves = "SELECT `tbl_savegames`.`game_id` FROM `tbl_savegames` WHERE `tbl_savegames`.`user_id` = $userIDNumber ORDER BY `tbl_savegames`.`game_id` ASC";
+  }
+  $existingSaves = $database->query($getExistingSaves)->fetchAll();
+  $countedSaves = count($existingSaves);
+  if (($countedSaves < 3) && ($saveNameLength == true) && ($save_nameNotExist == true))
+  {
+    foreach ($user_id as $uid) {
+      $userIDNumber = $uid["id"];
+    $inputSaveQuery = "INSERT INTO `tbl_savegames` (`game_id`, `user_id`, `game_name`, `date_created`, `date_last_opened`) VALUES (NULL, '$userIDNumber', '$save_name', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+  }
+    $database->query($inputSaveQuery);
+    $new_game_id = $database->lastInsertId();
+      $_SESSION['game'] = $new_game_id;
     ?>
     <script type='text/javascript'>
         setTimeout(function () {
