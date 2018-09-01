716a9ca6a8e00458c409fe3127dbfb2c1fc4af63
MrRedstoneWolf <stijn@sharks.nl>
2018-09-02 00:43:48 +0200

    4.6 Add Set Last_Opened To CURRENT_TIME At Close

--- app/QuitGameHandler.php
+++ app/QuitGameHandler.php
@@ -1,7 +1,10 @@
 <?php
 
+require('config/DatabaseConnector.php');
 session_start(); // session moet gek genoeg ALTIJD gestart worden.
-
+$game_id = $_SESSION['game'];
+  $input_last_opened = "UPDATE `tbl_savegames` SET `date_last_opened` = CURRENT_TIMESTAMP WHERE `tbl_savegames`.`game_id` = $game_id;";
+  $database->query($input_last_opened);
 $_SESSION['game'] = NULL;
 // Nu maken we de session null en vernietigen we de session
 
