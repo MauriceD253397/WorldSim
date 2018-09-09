97273c0cdf78d480e4e15987bb8330e7eedcc55c
MrRedstoneWolf <stijn@sharks.nl>
2018-09-09 15:37:01 +0200

    3.8.2 Add Version When Not Logged In

--- public/index.php
+++ public/index.php
@@ -99,6 +99,7 @@
                     <div></div>
 
                 </div>
+                <div class="game_version">Version: Alpha</div>
                 <?php
             }
                  ?>
